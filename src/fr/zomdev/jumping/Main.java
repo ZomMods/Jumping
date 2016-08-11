c7471330d652ea6f4845c70a15d8ecb2de925fe3
BuildTools <unconfigured@null.spigotmc.org>
2016-08-11 16:13:57 +0200

    First Commit

---
+++ src/fr/zomdev/jumping/Main.java
@@ -0,0 +1,15 @@
+package fr.zomdev.jumping;
+
+import fr.zomdev.jumping.frames.MenuFrame;
+
+import javax.swing.*;
+
+/**
+ * Created by Thomas on 11/08/2016.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        JFrame frame1 = new MenuFrame();
+    }
+}
---
+++ src/fr/zomdev/jumping/frames/MenuFrame.java
@@ -0,0 +1,14 @@
+package fr.zomdev.jumping.frames;
+
+import javax.swing.*;
+
+/**
+ * Created by Thomas on 11/08/2016.
+ */
+public class MenuFrame extends JFrame {
+
+    public MenuFrame(String title){
+        this.setTitle(title);
+        this.setVisible(true);
+    }
+}
